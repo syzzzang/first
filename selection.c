1291b4abfa93fe607ad98e4bd936e9962cb21dc1
syzzzang <syzz1105@naver.com>
2020-03-06 20:26:19 +0900

    des+asc

--- selection.c
+++ selection.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int* selectionsort(int *arr, int length){
+int* ascendingselectionsort(int *arr, int length){
     int temp=0;
-    int loc=0;
+    int min=0;
     for(int j=0;j<length-1;j++){
-        int min=j;
+        min=j;
         printf("%d번째\n",j+1);
         for(int i=j+1;i<length;i++){
             if (arr[min]>arr[i]){
@@ -24,12 +24,36 @@ int* selectionsort(int *arr, int length){
     }
     return arr;
 }
+int descendingselectionsort(int *arr, int length){
+    int temp=0;
+    int max=0;
+    for(int i=0;i<length-1;i++){
+        max=i;
+        printf("%d번째\n",max);
+        for(int j=i+1;j<length;j++){
+            if(arr[max]<arr[j]){
+                max=j;
+                printf("max: %d\n",arr[max]);
+            }
+        }
+        temp=arr[i];
+        arr[i]=arr[max];
+        arr[max]=temp;
+        printf("swap\n");
+        for(int i=0;i<length;i++){
+            printf("%d ",arr[i]);
+        }
+        printf("\n");
+    }
+}
 
 
 int main(){
     int arr[6]={5,2,4,6,1,3};
-    selectionsort(arr,6);
-    for(int i=0;i<6;i++){
-        printf("%d ",arr[i]);
-    }
+    int length=sizeof(arr)/sizeof(int);
+    ascendingselectionsort(arr,length);
+    printf("----------");
+    printf("\n");
+    descendingselectionsort(arr,length);
+    
 }
